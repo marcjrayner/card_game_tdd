f11d752237cb6c3f7f5b8bd990a46d2528262d4f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-09 14:01:27 +0000

    deck starts empty

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int getCardCount() {
+        return this.cards.size();
+    }
 }
