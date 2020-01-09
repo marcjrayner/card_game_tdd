2fa6296e2e07cc8a3f6566adeb27db156122d2d0
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-09 14:45:02 +0000

    adds player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    private ArrayList<Card> hand;
+
+    public Player() {
+        this.hand = new ArrayList<Card>();
+    }
+
+    public int handCardCount() {
+        return this.hand.size();
+    }
+
+
+    public void addCard(Card card) {
+        this.hand.add(card);
+    }
+}
---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+    private Player player;
+    private Card card;
+
+    @Before
+    public void before() {
+        player = new Player();
+        card = new Card(SuitType.HEARTS, CardValueType.TEN);
+    }
+
+    @Test
+    public void handStartsEmpty() {
+        assertEquals(0, player.handCardCount());
+    }
+
+    @Test
+    public void canAddCard() {
+        player.addCard(card);
+        assertEquals(1, player.handCardCount());
+    }
+
+
+}
